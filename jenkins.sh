0bff39d1568a9b2752eac256fb105b6bda0ce6fb
Tejapakanati <136108962+Tejapakanati@users.noreply.github.com>
2026-01-31 11:28:10 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 sudo yum upgrade
-sudo dnf install java-21-amazon-corretto
-sudo dnf install jenkins-y
+sudo dnf install java-21-amazon-corretto -y
+sudo dnf install jenkins -y
+systemctl restart jenkins
