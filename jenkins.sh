984958b8f153e863a65c82d3100b129bd7ac8b5f
Tejapakanati <136108962+Tejapakanati@users.noreply.github.com>
2026-02-03 10:45:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,15 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 sudo yum upgrade
-sudo dnf install java-11-amazon-corretto java-11-amazon-corretto-devel -y
+sudo dnf install java-17-amazon-corretto -y
 sudo dnf install jenkins -y
 systemctl restart jenkins
 sudo dnf install git -y
+
+//if incase problme with start jenkins //
+
+sudo systemctl daemon-reload
+sudo systemctl restart jenkins
+sudo systemctl status jenkins
+
 
